b47b2b09758e85caf03e92b3a8bde4a0aa421a79
AnobzII <r_parker19@hotmail.com>
2021-03-22 13:38:13 +0000

    changed links

--- udemy/js/game.js
+++ udemy/js/game.js
@@ -64,7 +64,7 @@ startGame = () => {
 getNewQuestion = () => {
     if(availableQuesions.length == 0 || questionCounter >= MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score);
-        return window.location.assign('/end.html');
+        return window.location.assign('udemy/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
