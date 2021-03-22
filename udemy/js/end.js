7b0f3986121de78c29f563cbe6a5c2beb41bb56e
AnobzII <r_parker19@hotmail.com>
2021-03-22 11:42:07 +0000

    final

--- udemy/js/end.js
+++ udemy/js/end.js
@@ -27,7 +27,7 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('udemy/');
+    window.location.assign('/');
 
     console.log(highScores);
 };
