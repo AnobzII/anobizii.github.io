b8bfb81cad0aab652be7aa03d7a899c5e6e3a8fc
AnobzII <r_parker19@hotmail.com>
2021-03-23 09:28:04 +0000

    Update game.js

--- udemy/js/game.js
+++ udemy/js/game.js
@@ -49,7 +49,7 @@ fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 //CONSTANTS
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questionCounter = 0;
